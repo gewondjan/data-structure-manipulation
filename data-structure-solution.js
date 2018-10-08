f4b8da5c180bcb645eb97d3dfba1eeb64269d1da
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2018-10-08 14:02:44 -0600

    read in the csv file

--- data-structure-solution.js
+++ data-structure-solution.js
@@ -1,1 +1,14 @@
 //Javascript file
+/* eslint-disable */
+
+//Include the needed tools
+var d3 = require('./d3/d3');
+var fs = require('fs');
+
+
+//Get the name of the csv file and put the contents of the file into a string variable
+var fileName = process.argv[2];
+var contents = fs.readFileSync(fileName).toString();
+
+
+console.log(d3.csvParse(contents));
